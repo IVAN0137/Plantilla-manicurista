fa55f186e0a422934af44bd25a99a76b04694c6f
Ivan <ivanmartinezgudino55@gmail.com>
2025-01-16 09:09:12 -0600

    segun se resolvio el problema de pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,21 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Plantilla-manicurista/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
+      '@': path.resolve(__dirname, './src')
+    }
   },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  server: {
+    port: 3000,
+    open: true
+  }
+})
